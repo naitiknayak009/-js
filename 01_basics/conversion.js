127087b91dc284707530a5fcf0e7494f0c9f2082
naitiknayak009 <149299732+naitiknayak009@users.noreply.github.com>
2024-03-26 14:28:00 +0000

    conversionofdatatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,28 @@
+let x = "Naitik"
+
+console.log(typeof x)
+console.log(typeof (x))
+
+let value = Number(x)
+console.log(typeof value)
+console.log(value)
+
+
+//"33" = 33
+//"33abc" => NaN
+
+let isloggedin = 1
+let boolean = Boolean(isloggedin)
+
+console.log(typeof(boolean));
+console.log(boolean);
+
+// 1=> true, 0=> false
+//"" => false
+//"hitesh" => true
+
+let num = 33
+let sn = String(num)
+
+console.log(sn);
+console.log(typeof sn);
